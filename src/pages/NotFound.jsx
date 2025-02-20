1687d4a1304f59511c3305f1e3d1da830c895bee
be <be@gmail.com>
2025-02-20 19:48:14 +0500

    ready

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
     <>
       <div className="not-found-container">
         <img src="/not-found.jpg" alt="" />
+      </div>
+
+      <div className="return-btn-container">
+        <Link to={"/"} className="not-found-btn">
+          RETURN TO HOME PAGE
+        </Link>
       </div>
     </>
   );
